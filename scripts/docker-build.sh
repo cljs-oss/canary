494bf4067ffc56c88ee9f23c0d1bf76731f4d0c6
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-11 01:50:49 +0200

    docker build --cache-from

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,8 +3,15 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 false && source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
 
+DOCKER_CACHE_FROM=${DOCKER_CACHE_FROM:-"$DOCKER_IMAGE_NAME:latest"}
+TRAVIS_COMMIT=${TRAVIS_COMMIT:-"local"}
+
 pushd "$DOCKER_DIR"
 
-docker build -t "$DOCKER_IMAGE_NAME" .
+docker build \
+  --cache-from "$DOCKER_CACHE_FROM" \
+  -t "$DOCKER_IMAGE_NAME:latest" \
+  -t "$DOCKER_IMAGE_NAME:$TRAVIS_COMMIT" \
+  .
 
 popd
