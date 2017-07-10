f905c431221d0b756e29c274c4d72d6589f513fe
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-10 22:00:04 +0200

    refine travis entrypoint, pass in job-id as TRAVIS_BUILD_NUMBER

--- scripts/travis-entrypoint.sh
+++ scripts/travis-entrypoint.sh
@@ -3,6 +3,9 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 false && source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
 
+TRAVIS=${TRAVIS}
+TRAVIS_BUILD_NUMBER=${TRAVIS_BUILD_NUMBER:-?}
+
 if [[ -z "$TRAVIS" ]]; then
   echoerr "travis-entrypoint.sh should be launched only in the context of travis CI"
   exit 77
@@ -14,13 +17,13 @@ JOB_ARGS=$(git show -s --format=%B "jobs" | head -n 1)
 
 # TODO: do some better sanitization of JOB_ARGS
 if [[ ! ${JOB_ARGS} == job* ]]; then
-  echoerr "the commit message to jobs must begin with 'job' and follow by job arguments"
-  echoerr "I see a commit message '$JOB_ARGS'"
+  echoerr "The commit message to jobs must begin with 'job' and follow by job arguments."
+  echoerr "I see a commit message '$JOB_ARGS'."
   exit 1
 fi
 
 ./scripts/docker-build.sh
 
-./scripts/docker-run.sh ${JOB_ARGS} --production
+./scripts/docker-run.sh ${JOB_ARGS} --job-id "$TRAVIS_BUILD_NUMBER" --production
 
 popd
