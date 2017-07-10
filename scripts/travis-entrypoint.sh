e4c6de352f431bf6f209b56f82b446ca2f60c0f4
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-10 21:26:04 +0200

    add basic functionality to travis entrypoint

--- scripts/travis-entrypoint.sh
+++ scripts/travis-entrypoint.sh
@@ -3,15 +3,24 @@
 source "$(dirname "${BASH_SOURCE[0]}")/_config.sh"
 false && source _config.sh # never executes, this is here just for IntelliJ Bash support to understand our sourcing
 
+if [[ -z "$TRAVIS" ]]; then
+  echoerr "travis-entrypoint.sh should be launched only in the context of travis CI"
+  exit 77
+fi
+
 pushd "$ROOT"
+
+JOB_ARGS=$(git show -s --format=%B "jobs" | head -n 1)
+
+# TODO: do some better sanitization of JOB_ARGS
+if [[ ! ${JOB_ARGS} == job* ]]; then
+  echoerr "the commit message to jobs must begin with 'job' and follow by job arguments"
+  echoerr "I see a commit message '$JOB_ARGS'"
+  exit 1
+fi
 
 ./scripts/docker-build.sh
 
-# TODO: extract CLOJURESCRIPT_VERSION from env, url args or git commit message
-# TODO: extract additional args for the job
-
-CLOJURESCRIPT_VERSION=${CLOJURESCRIPT_VERSION:-1.2.3}
-
-./scripts/docker-run.sh job -i "$CLOJURESCRIPT_VERSION"
+./scripts/docker-run.sh ${JOB_ARGS} --production
 
 popd
