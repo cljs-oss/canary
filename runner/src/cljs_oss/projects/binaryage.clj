3f6dfe789ee4d9392c347790f46c78905fbf5ee4
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-12 01:33:43 +0200

    cljs-devtools: use CANARY_CLJS_DEVTOOLS_TRAVIS_TOKEN

--- runner/src/cljs_oss/projects/binaryage.clj
+++ runner/src/cljs_oss/projects/binaryage.clj
@@ -2,4 +2,4 @@
   (:require [cljs-oss.tools.support :refer [trigger-travis-build!]]))
 
 (defn ^:task cljs-devtools [options]
-  (trigger-travis-build! "binaryage/cljs-devtools" "CLJS_DEVTOOLS_TRAVIS_TOKEN" options))
+  (trigger-travis-build! "binaryage/cljs-devtools" "CANARY_CLJS_DEVTOOLS_TRAVIS_TOKEN" options))
