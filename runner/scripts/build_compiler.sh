76f2301f0ba149d24d3e4a36266184c0c44e4d3c
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-13 21:14:58 +0200

    update compiler build script

--- runner/scripts/build_compiler.sh
+++ runner/scripts/build_compiler.sh
@@ -2,6 +2,14 @@
 
 set -e -o pipefail
 
+ANSI_CLEAR="\033[0K"
+
+travis_fold() {
+  local action=$1
+  local name=$2
+  echo -en "travis_fold:${action}:${name}\r${ANSI_CLEAR}"
+}
+
 # expected environment
 COMPILER_REV=${COMPILER_REV:-"master"} # https://git-scm.com/book/tr/v2/Git-Tools-Revision-Selection
 COMPILER_REPO=${COMPILER_REPO:-"https://github.com/clojure/clojurescript.git"}
@@ -11,6 +19,14 @@ echo "going to build $COMPILER_REV from $COMPILER_REPO"
 
 if [[ "$CANARY_VERBOSITY" -gt 0 ]]; then
   mvn --version
+fi
+
+GIT_VERBOSITY="--quiet"
+if [[ "$CANARY_VERBOSITY" -gt 0 ]]; then
+  GIT_VERBOSITY=""
+fi
+if [[ "$CANARY_VERBOSITY" -gt 1 ]]; then
+  GIT_VERBOSITY="--verbose"
 fi
 
 # cwd should already be in a temporary folder, see build.clj
@@ -28,25 +44,29 @@ git init
 git remote add origin "$COMPILER_REPO"
 # unfortunately we have to fetch whole clojurescript repo, build script depends on it
 # TODO: we probably want to setup sharing of git objects persisted by travis to speed this up
-git fetch --tags origin "$COMPILER_REV"
+git fetch ${GIT_VERBOSITY} --tags origin "$COMPILER_REV"
 git checkout FETCH_HEAD -b canary-build
 
-git log -1 --pretty=oneline
+echo "Effective ClojureScript SHA to be built:"
+git log -1 --pretty=format:"%C(magenta)%h%C(reset) | %C(yellow)%s%C(reset) | %C(blue)%an%C(reset) | %ad" --date=rfc
+echo
 
-# we want to silence maven output in clojurescript build unless invoker wants us go extra verbose
-export MAVEN_OPTS="-Dorg.slf4j.simpleLogger.defaultLogLevel=warn -Dorg.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn"
-if [[ "$CANARY_VERBOSITY" -gt 1 ]]; then
-  export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=info
-fi
-if [[ "$CANARY_VERBOSITY" -gt 2 ]]; then
-  export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.defaultLogLevel=debug
+if [[ "$CANARY_VERBOSITY" -eq 0 ]]; then
+  export CLJS_SCRIPT_QUIET=1
+  MAVEN_VERBOSITY="--quiet"
 fi
 
+export CLJS_SCRIPT_MVN_OPTS="--batch-mode $MAVEN_VERBOSITY"
+
+travis_fold start clojurescript-build
+echo "Building ClojureScript compiler..."
+
 if [[ "$CANARY_VERBOSITY" -gt 2 ]]; then
-  echo "effective environment before compiler build:"
+  echo "Effective environment before compiler build:"
   env
 fi
 
 ./script/build
+travis_fold end clojurescript-build
 
 pwd
