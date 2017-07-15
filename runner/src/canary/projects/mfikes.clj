38315c6056067d28e8898d620e5e2ba8f9a58433
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-15 22:07:56 +0200

    add planck task
    @mfikes ^

---
+++ runner/src/canary/projects/mfikes.clj
@@ -0,0 +1,5 @@
+(ns canary.projects.mfikes
+  (:require [canary.runner.travis :as travis]))
+
+(defn ^:task planck [options]
+  (travis/trigger-build! "mfikes/planck" "CANARY_PLANCK_TRAVIS_TOKEN" "master" options))
