5efb6cf64fbd543626b37f7ae89a3c2ed1861a6d
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-15 23:22:24 +0200

    Revert "try to limit planck matrix to linux-clang only"
    This reverts commit 67ff99da0c5da9e711bbab7c0edf47c75b8fcd0f.

--- runner/src/canary/projects/mfikes.clj
+++ runner/src/canary/projects/mfikes.clj
@@ -2,7 +2,4 @@
   (:require [canary.runner.travis :as travis]))
 
 (defn ^:task planck [options]
-  (let [config-overrides {:matrix {:include [{:os       "linux"
-                                              :compiler "clang"}]}}
-        options (assoc options :travis-body {:request {:config config-overrides}})]
-    (travis/trigger-build! "mfikes/planck" "CANARY_PLANCK_TRAVIS_TOKEN" "master" options)))
+  (travis/trigger-build! "mfikes/planck" "CANARY_PLANCK_TRAVIS_TOKEN" "master" options))
