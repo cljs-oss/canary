ab7f10a155184355b64369e7f6f39aa19cfabf1f
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-15 23:13:14 +0200

    polish docker-load.sh a bit more

--- scripts/docker-load.sh
+++ scripts/docker-load.sh
@@ -4,23 +4,23 @@ source "$(dirname "${BASH_SOURCE[0]}")/_config.sh" || true || source _config.sh 
 source "$(dirname "${BASH_SOURCE[0]}")/lib/travis.sh" || true || source lib/travis.sh # this is here just for IntelliJ Bash support to understand our sourcing
 
 travis_fold start docker-load
+echo "Load canary docker image from cache"
+
 function finish {
   travis_fold end docker-load
 }
 trap finish EXIT
 
-echo "Load docker image from cache"
-
 if [[ ! -f "$DOCKER_CACHE_FILE" ]]; then
-  echo "  cache file not found"
+  echo "Canary docker cache file not found"
   exit 0
 fi
 
-echo "images before:"
+echo "Docker images before:"
 docker images -a
 
-echo "  loading cache file: $DOCKER_CACHE_FILE"
+echo "Loading cache file: $DOCKER_CACHE_FILE"
 docker load -q -i "$DOCKER_CACHE_FILE"
 
-echo "images after:"
+echo "Docker images after:"
 docker images -a
