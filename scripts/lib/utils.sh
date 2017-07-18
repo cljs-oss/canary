b728ee84cefc1207fb7a41d44a8ecc6502736c0e
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-18 20:59:10 +0200

    remove portable_realpath, not used anymore

--- scripts/lib/utils.sh
+++ scripts/lib/utils.sh
@@ -8,19 +8,6 @@ popd () {
     command popd "$@" > /dev/null
 }
 
-portable_realpath() {
-  case "$OSTYPE" in
-    darwin*)
-      if [ -z "$(which grealpath)" ]; then
-        echo "grealpath needed under OSX, please \`brew install coreutils\`"
-        exit 1
-      fi
-      grealpath "$@" ;;
-    *)
-      realpath "$@" ;;
-  esac
-}
-
 echoerr() {
   printf "\e[31m%s\e[0m\n" "$*" >&2;
 }
