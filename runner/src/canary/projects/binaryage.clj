83c31e4722f781a2c7cfdaf0481aef2458a2425b
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-19 19:36:02 +0200

    add binaryage/chromex as a new task

--- runner/src/canary/projects/binaryage.clj
+++ runner/src/canary/projects/binaryage.clj
@@ -3,3 +3,6 @@
 
 (defn ^:task cljs-devtools [options]
   (travis/request-build! "binaryage/cljs-devtools" "CANARY_CLJS_DEVTOOLS_TRAVIS_TOKEN" options))
+
+(defn ^:task chromex [options]
+  (travis/request-build! "binaryage/chromex" "CANARY_CHROMEX_TRAVIS_TOKEN" options))
