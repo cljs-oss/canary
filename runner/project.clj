7706e6379d6aef744604b86f0b5642ec2509b678
Antonin Hildebrand <antonin@hildebrand.cz>
2017-07-20 21:33:42 +0200

    project: bump core.async to 0.3.443

--- runner/project.clj
+++ runner/project.clj
@@ -2,7 +2,7 @@
   :description "TODO"
   :url "TODO"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.442"]
+                 [org.clojure/core.async "0.3.443"]
                  [org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.namespace "0.2.11"]
                  [org.clojure/data.json "0.2.6"]
