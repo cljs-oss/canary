cljs-oss/canary

commit c398d7652cb5d2519d4a97c1cff61bcff048be7d

runner: implement spawning delay in launch-tasks!

runner/src/canary/runner/jobs.clj
@@ -44,7 +44,10 @@
 (defn launch-tasks! [tasks options]
   (let [enabled-tasks (filter :enabled tasks)
         * (fn [tasks task]
-            (assoc tasks (launch-task! task options) (assoc task :running true)))]
+            (assoc tasks (launch-task! task options) (assoc task :running true))
+            (let [delay (:spawning-delay options)]
+              (if (pos? delay)
+                (Thread/sleep delay))))]
     (doall (reduce * {} enabled-tasks))))
 
 (defn report-disabled-tasks [tasks options]

commit 63a29f0496461e96c3f88344f158e18bed9344ae

runner: fix "early completion" bug introduced in c398d7652cb5d2519d4a97c1cff61bcff048be7d

runner/src/canary/runner/jobs.clj
@@ -44,10 +44,11 @@
 (defn launch-tasks! [tasks options]
   (let [enabled-tasks (filter :enabled tasks)
         * (fn [tasks task]
-            (assoc tasks (launch-task! task options) (assoc task :running true))
-            (let [delay (:spawning-delay options)]
-              (if (pos? delay)
-                (Thread/sleep delay))))]
+            (let [updated-tasks (assoc tasks (launch-task! task options) (assoc task :running true))]
+              (let [delay (:spawning-delay options)]
+                (if (pos? delay)
+                  (Thread/sleep delay)))
+              updated-tasks))]
     (doall (reduce * {} enabled-tasks))))
 
 (defn report-disabled-tasks [tasks options]