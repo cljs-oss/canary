918a169386adacebefdf23b710f875b4412107b9
Antonin Hildebrand <antonin@hildebrand.cz>
2018-01-30 02:05:28 +0100

    make auto-job script more flexible, move workdir under .canary-jobs-branch
    The goal is to persuade git to use babot's credential when both authoring and commiting into the jobs branch.

--- scripts/launchd/auto-job.sh
+++ scripts/launchd/auto-job.sh
@@ -15,14 +15,32 @@ trap finish EXIT
 # ensure we start in canary's root folder
 cd "$(dirname "${BASH_SOURCE[0]}")/../..";
 
-cd ..
+AUTO_JOB_CONFIG_FILE=~/.canary-auto-job-config.sh
+
+if [[ -f "${AUTO_JOB_CONFIG_FILE}" ]]; then
+  echo "sourcing ${AUTO_JOB_CONFIG_FILE}..."
+  source "${AUTO_JOB_CONFIG_FILE}"
+fi
+
+CANARY_AUTO_JOB_REPO_TOKEN=${CANARY_AUTO_JOB_REPO_TOKEN}
+CANARY_AUTO_JOB_COMMIT_AUTHOR=${CANARY_AUTO_JOB_COMMIT_AUTHOR}
+
+if [[ -z "$CANARY_AUTO_JOB_REPO_TOKEN" ]]; then
+  REPO_URL="git@github.com:cljs-oss/canary.git"
+else
+  REPO_URL="https://${CANARY_AUTO_JOB_REPO_TOKEN}@github.com/cljs-oss/canary.git"
+fi
+
+if [[ ! -z "$CANARY_AUTO_JOB_COMMIT_AUTHOR" ]]; then
+  GIT_COMMIT_AUTHOR="--author=\"${CANARY_AUTO_JOB_COMMIT_AUTHOR}\""
+fi
 
 # we keep another working copy for jobs branch, see https://github.com/cljs-oss/canary/tree/jobs
-JOB_BRANCH_FOLDER="canary-jobs-branch"
+JOB_BRANCH_FOLDER=".canary-jobs-branch"
 
 # clone it if it does not exist yet
 if [[ ! -d "$JOB_BRANCH_FOLDER" ]]; then
-  git clone --branch jobs git@github.com:cljs-oss/canary.git ${JOB_BRANCH_FOLDER}
+  git clone --branch jobs "${REPO_URL}" "${JOB_BRANCH_FOLDER}"
 fi
 
 cd ${JOB_BRANCH_FOLDER}
@@ -30,5 +48,11 @@ cd ${JOB_BRANCH_FOLDER}
 git fetch
 git reset --hard origin/jobs
 # TODO: we could check if there were recent job requests and skip if hot
-git commit -m "job" --allow-empty --author="BinaryAge Bot <bot@binaryage.com>"
+# bash COMMIT_AUTHOR might contain spaces and bash escaping rules are crazy, have to branch
+if [[ ! -z "$GIT_COMMIT_AUTHOR" ]]; then
+  git commit -m "job" --allow-empty "${GIT_COMMIT_AUTHOR}"
+else
+  git commit -m "job" --allow-empty
+fi
+
 git push
