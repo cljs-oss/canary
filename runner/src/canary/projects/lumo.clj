88d06b3f0bc18f6f0105a8e6410dc488bd96780a
Andrea Richiardi <a.richiardi.work@gmail.com>
2018-02-20 14:56:03 -0800

    Fix the lumo task

--- runner/src/canary/projects/lumo.clj
+++ runner/src/canary/projects/lumo.clj
@@ -2,4 +2,4 @@
   (:require [canary.runner.travis :as travis]))
 
 (defn ^:task lumo [options]
-  (travis/request-build! "amonteiro/lumo" "CANARY_LUMO_TRAVIS_TOKEN" options))
+  (travis/request-build! "anmonteiro/lumo" "CANARY_LUMO_TRAVIS_TOKEN" options))
