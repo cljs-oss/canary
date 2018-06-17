69ec5bd26695e73a8dae1730783057417dba27a0
Mike Fikes <mike@fikesfarm.com>
2018-06-17 16:44:42 -0400

    Spit closurelatest into closurecomp and closurelib

--- runner/src/canary/projects/mfikes.clj
+++ runner/src/canary/projects/mfikes.clj
@@ -27,5 +27,8 @@
 #_(defn ^:task andare [options]
   (travis/request-build! "mfikes/andare" "CANARY_ANDARE_TRAVIS_TOKEN" options))
 
-(defn ^:task closurelatest [options]
-  (travis/request-build! "mfikes/closurelatest" "CANARY_CLOSURELATEST_TRAVIS_TOKEN" options))
+(defn ^:task closurecomp [options]
+  (travis/request-build! "mfikes/closurecomp" "CANARY_CLOSURELATEST_TRAVIS_TOKEN" options))
+
+(defn ^:task closurelib [options]
+  (travis/request-build! "mfikes/closurelib" "CANARY_CLOSURELATEST_TRAVIS_TOKEN" options))
