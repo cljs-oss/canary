c2e00f299b8748ee009768bb66ba5ebd2b6e09c2
Antonin Hildebrand <antonin@hildebrand.cz>
2018-06-26 20:59:32 +0200

    remove env-config library
    It is not really that important library to be tested with Canary.

--- runner/src/canary/projects/binaryage.clj
+++ runner/src/canary/projects/binaryage.clj
@@ -10,8 +10,5 @@
 (defn ^:task cljs-oops [options]
   (travis/request-build! "binaryage/cljs-oops" "CANARY_CLJS_OOPS_TRAVIS_TOKEN" options))
 
-(defn ^:task env-config [options]
-  (travis/request-build! "binaryage/env-config" "CANARY_ENV_CONFIG_TRAVIS_TOKEN" options))
-
 (defn ^:task cljs-zones [options]
   (travis/request-build! "binaryage/cljs-zones" "CANARY_CLJS_ZONES_TRAVIS_TOKEN" options))
