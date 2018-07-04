304f619d1c52ba84fe92f91fd5b7c7d7f28645de
Mike Fikes <mike@fikesfarm.com>
2018-07-04 11:24:54 -0400

    Collapse to a single coal-mine task
    (Parallelization is handled on the coal-mine side)

--- runner/src/canary/projects/mfikes.clj
+++ runner/src/canary/projects/mfikes.clj
@@ -1,28 +1,8 @@
 (ns canary.projects.mfikes
   (:require [canary.runner.travis :as travis]))
 
-(defn add-partition-filter [options filter]
-  (assoc options :travis-config {:env {:global {"PARTITION_FILTER" filter}}}))
-
-(defn ^:task coal-mine-1 [options]
-  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN"
-    (add-partition-filter options "1")))
-
-(defn ^:task coal-mine-2 [options]
-  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN"
-    (add-partition-filter options "2")))
-
-(defn ^:task coal-mine-3 [options]
-  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN"
-    (add-partition-filter options "3")))
-
-(defn ^:task coal-mine-4 [options]
-  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN"
-    (add-partition-filter options "4")))
-
-(defn ^:task coal-mine-5 [options]
-  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN"
-    (add-partition-filter options "5")))
+(defn ^:task coal-mine [options]
+  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN" options))
 
 #_(defn ^:task andare [options]
   (travis/request-build! "mfikes/andare" "CANARY_ANDARE_TRAVIS_TOKEN" options))
