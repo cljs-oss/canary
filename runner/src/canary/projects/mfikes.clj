6d877be3eb388fa39bc5e516c37e04b2a7a478b6
Mike Fikes <mike@fikesfarm.com>
2018-07-04 11:46:46 -0400

    Specify the coal-mine job matrix

--- runner/src/canary/projects/mfikes.clj
+++ runner/src/canary/projects/mfikes.clj
@@ -2,7 +2,13 @@
   (:require [canary.runner.travis :as travis]))
 
 (defn ^:task coal-mine [options]
-  (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN" options))
+  (let [config-overrides {:env {:matrix ["PARTITION_FILTER=1"
+                                         "PARTITION_FILTER=2"
+                                         "PARTITION_FILTER=3"
+                                         "PARTITION_FILTER=4"
+                                         "PARTITION_FILTER=5"]}}
+        options (assoc options :travis-config config-overrides)]
+    (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN" options)))
 
 #_(defn ^:task andare [options]
   (travis/request-build! "mfikes/andare" "CANARY_ANDARE_TRAVIS_TOKEN" options))
