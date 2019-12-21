318ffff24c89f752a9a68d12aec5b614758efd5b
Antonin Hildebrand <antonin@hildebrand.cz>
2019-12-21 16:22:05 +0100

    runner: bump deps

--- runner/project.clj
+++ runner/project.clj
@@ -2,14 +2,14 @@
   :description "A tool for smoke-testing ClojureScript projects with a pinned ClojureScript version."
   :url "https://github.com/cljs-oss/canary"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/core.async "0.4.500"]
+                 [org.clojure/core.async "0.6.532"]
                  [org.clojure/tools.cli "0.4.2"]
-                 [org.clojure/tools.namespace "0.3.0"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.namespace "0.3.1"]
+                 [org.clojure/data.json "0.2.7"]
                  [me.raynes/conch "0.8.0"]
                  [me.raynes/fs "1.4.6"]
                  [clansi "1.0.0"]
-                 [funcool/cuerdas "2.2.0"]
+                 [funcool/cuerdas "2.2.1"]
                  [indole "1.0.0"]]
   :source-paths ["src"]
   :test-paths ["test/src" "test/fixtures/projects"]
