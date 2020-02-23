eac41d4acf56390567e28b5e6cd61f07daec76d2
Antonin Hildebrand <antonin@hildebrand.cz>
2020-02-23 12:52:22 +0100

    rgen: bump deps

--- rgen/project.clj
+++ rgen/project.clj
@@ -2,7 +2,7 @@
   :description "A tool for generating readme overview from canary reports."
   :url "https://github.com/cljs-oss/canary"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.cli "0.4.2"]
+                 [org.clojure/tools.cli "1.0.194"]
                  [funcool/cuerdas "2.2.1"]
                  [clansi "1.0.0"]
                  [me.raynes/fs "1.4.6"]]
