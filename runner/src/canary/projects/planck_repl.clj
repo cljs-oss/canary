a73c85d79c06da0cbbf0a99bbd53690aa54c1d99
Mike Fikes <mike@fikesfarm.com>
2021-05-22 08:36:30 -0400

    Exclude Planck macOS builds

--- runner/src/canary/projects/planck_repl.clj
+++ runner/src/canary/projects/planck_repl.clj
@@ -2,4 +2,6 @@
   (:require [canary.runner.travis :as travis]))
 
 (defn ^{:task true :homepage "planck-repl/planck"} planck [options]
-  (travis/request-build! "planck-repl/planck" "CANARY_PLANCK_TRAVIS_TOKEN" options))
+  (let [config-overrides {:jobs {:exclude [{:os "osx"}]}}
+        options (assoc options :travis-config config-overrides)]
+    (travis/request-build! "planck-repl/planck" "CANARY_PLANCK_TRAVIS_TOKEN" options)))
