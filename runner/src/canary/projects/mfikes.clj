878291118ddbe593f6fea7304584bcf7e8e8ae59
Mike Fikes <mike@fikesfarm.com>
2021-05-29 15:58:37 -0400

    Remove commented jobs in mfikes

--- runner/src/canary/projects/mfikes.clj
+++ runner/src/canary/projects/mfikes.clj
@@ -10,16 +10,5 @@
         options (assoc options :travis-config config-overrides)]
     (travis/request-build! "mfikes/coal-mine" "CANARY_COAL_MINE_TRAVIS_TOKEN" options)))
 
-#_(defn ^{:task true :homepage "mfikes/andare"} andare [options]
-  (travis/request-build! "mfikes/andare" "CANARY_ANDARE_TRAVIS_TOKEN" options))
-
-; Disable until CLJS-3064 fixed
-#_(defn ^{:task true :homepage "mfikes/closurecomp"} closurecomp [options]
-  (travis/request-build! "mfikes/closurecomp" "CANARY_CLOSURELATEST_TRAVIS_TOKEN" options))
-
-; Disable until ClojureScript upgrades to a compatible Closure Compiler
-#_(defn ^{:task true :homepage "mfikes/closurelib"} closurelib [options]
-  (travis/request-build! "mfikes/closurelib" "CANARY_CLOSURELATEST_TRAVIS_TOKEN" options))
-
 (defn ^{:task true :homepage "mfikes/cljs-bean"} cljs-bean [options]
   (travis/request-build! "mfikes/cljs-bean" "CANARY_CLJS_BEAN_TRAVIS_TOKEN" options))
