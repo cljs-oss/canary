c18b21b7749eff45f19265cfc7a39d21a7cd3018
Mike Fikes <mike@fikesfarm.com>
2021-05-29 15:57:17 -0400

    Remove lumo namespace

--- runner/src/canary/projects/lumo.clj
+++
@@ -1,5 +0,0 @@
-(ns canary.projects.lumo
-  (:require [canary.runner.travis :as travis]))
-
-#_(defn ^{:task true :homepage "anmonteiro/lumo"} lumo [options]
-  (travis/request-build! "anmonteiro/lumo" "CANARY_LUMO_TRAVIS_TOKEN" options))
