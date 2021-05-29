b5c7a773aed4ddc90b4d4e20406606b68401fc13
Mike Fikes <mike@fikesfarm.com>
2021-05-29 15:57:43 -0400

    Remove cljsrn namespace

--- runner/src/canary/projects/cljsrn.clj
+++
@@ -1,5 +0,0 @@
-(ns canary.projects.cljsrn
-  (:require [canary.runner.travis :as travis]))
-
-(defn ^{:task true :homepage "cljsrn/side-fx"} side-fx [options]
-  (travis/request-build! "cljsrn/side-fx" "CANARY_SIDE_FX_TRAVIS_TOKEN" options))
