d6d1d67adbb4e07504b60e144b946b294ed49675
Antonin Hildebrand <antonin@hildebrand.cz>
2021-05-29 15:09:56 +0200

    runner: replace all my travis tokens with single CANARY_DARWINS_TRAVIS_TOKEN
    For easier maintenance.

--- runner/src/canary/projects/binaryage.clj
+++ runner/src/canary/projects/binaryage.clj
@@ -2,13 +2,13 @@
   (:require [canary.runner.travis :as travis]))
 
 (defn ^{:task true :homepage "binaryage/cljs-devtools"} cljs-devtools [options]
-  (travis/request-build! "binaryage/cljs-devtools" "CANARY_CLJS_DEVTOOLS_TRAVIS_TOKEN" options))
+  (travis/request-build! "binaryage/cljs-devtools" "CANARY_DARWINS_TRAVIS_TOKEN" options))
 
 (defn ^{:task true :homepage "binaryage/chromex"} chromex [options]
-  (travis/request-build! "binaryage/chromex" "CANARY_CHROMEX_TRAVIS_TOKEN" options))
+  (travis/request-build! "binaryage/chromex" "CANARY_DARWINS_TRAVIS_TOKEN" options))
 
 (defn ^{:task true :homepage "binaryage/cljs-oops"} cljs-oops [options]
-  (travis/request-build! "binaryage/cljs-oops" "CANARY_CLJS_OOPS_TRAVIS_TOKEN" options))
+  (travis/request-build! "binaryage/cljs-oops" "CANARY_DARWINS_TRAVIS_TOKEN" options))
 
 (defn ^{:task true :homepage "binaryage/cljs-zones"} cljs-zones [options]
-  (travis/request-build! "binaryage/cljs-zones" "CANARY_CLJS_ZONES_TRAVIS_TOKEN" options))
+  (travis/request-build! "binaryage/cljs-zones" "CANARY_DARWINS_TRAVIS_TOKEN" options))
